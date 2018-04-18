2eaa264d3604b71e9883c09dab66a1888624b394
matsudamper <matsudamper971227@gmail.com>
2018-04-19 01:47:51 +0900

    誤植修正

--- keyboards/ergodox_ez/keymaps/my_custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/my_custom/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_COMM,
         KC_LCTRL,       KC_LGUI,      KC_LALT,KC_LEFT,KC_RGHT,
-                                               KC_PMNS,       KC_LGUI,
+                                               KC_MINS,       KC_LGUI,
                                                               KC_PGUP,
                                                KC_SPC,KC_LBRC,KC_HOME,
         // right hand
