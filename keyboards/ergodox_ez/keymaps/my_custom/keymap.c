65d0d5b38cfaef661b694411a20560bb82563d12
matsudamper <let@matsudamper.net>
2018-05-05 05:35:55 +0900

    変更

--- keyboards/ergodox_ez/keymaps/my_custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/my_custom/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  '   |           |  ;   |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Layer  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   /  | Enter  |
- * |--------+------+------+------+------+------|  ]   |           |  [   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ;  |   '  |   ↑  |   \    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | CTRL | Win  |      | Alt  | CTRL |                                       | Jan  |      |   ←  |   ↓  |   →  |
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_QUOT,
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RBRC,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTRL,       KC_LGUI,      KC_NO,  KC_LALT,KC_LCTRL,
                                                       KC_MINS, KC_PGUP,
                                                                KC_NO,
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGDN,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,      KC_BSPC,
              KC_SCLN,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,      KC_MINS,
                           KC_H,   KC_J,   KC_K,    KC_L,    KC_SLSH,   KC_ENT,
-             KC_LBRC,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_UP,     KC_BSLS,
+             KC_RBRC,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_UP,     KC_BSLS,
                                   JAN,    KC_NO,   KC_LEFT, KC_DOWN,   KC_RIGHT,
              KC_PGDN,KC_EQL,
              KC_NO,
