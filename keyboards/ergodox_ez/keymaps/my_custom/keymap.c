2d2b9fee53eb2e3c819cf4b46d1b90119b128833
matsudamper <let@matsudamper.net>
2019-05-30 04:41:48 +0900

    update keymap

--- keyboards/ergodox_ez/keymaps/my_custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/my_custom/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |TABPRE|           |TABNEX|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |TABNEX|           |TABPRE|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  '   |           |  ;   |   Y  |   U  |   I  |   O  |   P  | B_space|
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | CTRL | Win  |      | Alt  | CTRL |                                       | Jan  |      |   ←  |   ↓  |   →  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |   -  | PgUp |       | PgDn |   =   |
+ *                                        |   -  |      |       | PgUp |   =   |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      |      |       | PgDn |        |      |
  *                                 | Space|  ,   |------|       |------|   .    |   -  |
  *                                 |      |      | End  |       | Home |        |      |
  *                                 `--------------------'       `----------------------'
@@ -51,22 +51,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TAB_PREV,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TAB_NEXT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_QUOT,
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTRL,       KC_LGUI,      KC_NO,  KC_LALT,KC_LCTRL,
-                                                             KC_MINS, KC_PGUP,
+                                                             KC_MINS, KC_NO,
                                                                       KC_NO,
                                                      KC_SPC, KC_COMM, KC_END,
         // right hand
-        TAB_NEXT,    KC_6,   KC_7,   KC_8,    KC_9,    KC_0,      KC_MINS,
+        TAB_PREV,    KC_6,   KC_7,   KC_8,    KC_9,    KC_0,      KC_MINS,
         KC_SCLN,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,      KC_BSPC,
                      KC_H,   KC_J,   KC_K,    KC_L,    KC_SLSH,   KC_ENT,
         KC_RBRC,     KC_N,   KC_M,   KC_NO,   KC_NO,   KC_UP,     WIN_TAB,
                              JAN,    KC_NO,   KC_LEFT, KC_DOWN,   KC_RIGHT,
-        KC_PGDN, KC_EQL,
-        KC_NO,
+        KC_PGUP, KC_EQL,
+        KC_PGDN,
         KC_HOME, KC_DOT,  KC_MINS
     ),
 /* Keymap 1: Symbol Layer
@@ -189,9 +189,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_L:
       if (record->event.pressed) {
         if (IS_WIN) {
-          SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LGUI)SS_TAP(X_LEFT)SS_UP(X_LGUI)SS_UP(X_LCTRL)); 
+          SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LGUI)SS_TAP(X_LEFT)SS_UP(X_LGUI)SS_UP(X_LCTRL));
         } else {
-          SEND_STRING(SS_DOWN(X_LGUI)SS_TAP(X_LEFT)SS_UP(X_LGUI)); 
+          SEND_STRING(SS_DOWN(X_LGUI)SS_TAP(X_LEFT)SS_UP(X_LGUI));
         }
       }
       return false;
